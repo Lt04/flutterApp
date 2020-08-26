2c4abcb3eae8ed1a4b8f472143c2b5db454734d1
Kuba <jakub.k.borowiak@student.put.poznan.pl>
2020-08-26 12:45:22 +0200

    qr code scanner

--- test_drive/lib/list.dart
+++ test_drive/lib/list.dart
@@ -1,15 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_icons/flutter_icons.dart';
-import 'package:test_drive/ontap.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 import 'package:wifi/wifi.dart';
 import 'package:connectivity/connectivity.dart';
 import 'dart:async';
 import 'package:test_drive/actions.dart';
-import 'package:fluttertoast/fluttertoast.dart';
-
+import 'package:barcode_scan/barcode_scan.dart';
 
 class PasteList extends StatefulWidget{
 
@@ -18,15 +14,9 @@ class PasteList extends StatefulWidget{
 }
 
 class ListState extends State<PasteList>{
-  List<Album> lista = [];
-
-  ScrollController _controller;
-  int start = 0;
 
   @override
   void initState() {
-    _controller = ScrollController();
-    _controller.addListener(_scrollListener);
     super.initState();
     Timer.periodic(Duration(seconds: 1), (Timer t){
       setState(() { 
@@ -37,15 +27,9 @@ class ListState extends State<PasteList>{
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      body: wifiList()
+      body: wifiList(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
-  }
-
-  _scrollListener(){
-    if(_controller.offset >= _controller.position.maxScrollExtent && !_controller.position.outOfRange){
-      start += 10;
-      setState((){});
-    }
   }
 
   wifiModal(String name, String message){
@@ -110,6 +94,15 @@ class ListState extends State<PasteList>{
                  },
                );
              }
+             else if(snapshot.data != null && snapshot.data.length == i+1){
+               return FloatingActionButton.extended(
+                        onPressed: _scanQR,
+                        icon: Icon(Icons.camera_alt),
+                        backgroundColor: Colors.green,
+                        label: Text("Scan QR code"),
+                        
+                      );
+             }
              else if(snapshot.data != null && snapshot.data.length > i){
                return Container();
              }
@@ -119,52 +112,23 @@ class ListState extends State<PasteList>{
     );
   }
 
-  Widget listFromInternet(){
-    return FutureBuilder(
-      builder: (context, projectSnap){
-        if(projectSnap.data == null || projectSnap.data.length == 0){
-          return Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-              IconButton(
-                icon: Icon(EvilIcons.refresh, size: 30.0),
-                onPressed: (){
-                  setState((){
-                  });
-                }
-              ),
-              Text("Refresh")
-            ])],
-          );
-        }else{
-          return ListView.builder(
-            controller: _controller,
-            itemBuilder: (context, i){
-              if(projectSnap.data.length > i){
-                return ListTile(
-                  subtitle: Text(projectSnap.data[i].title),
-                  title: Text(projectSnap.data[i].id.toString()),
-                  leading: Icon(FontAwesome.gamepad, color: Colors.green),
-                  onTap: (){
-                    Navigator.of(context).push(
-                      MaterialPageRoute(builder: (context)=>Ontap(
-                        title: projectSnap.data[i].id.toString(),
-                      ))
-                    ); 
-                  }
-                );
-              }
-            }
-          );
+  Future _scanQR() async{
+    try{
+      ScanResult qrResult = await BarcodeScanner.scan();
+      Connectivity().getWifiName().then((name){
+        if(name == null){
+          wifiModal(qrResult.rawContent, "");
         }
-      },
-      future: fetchAlbum()
-    );
+        else if(name != null && name == qrResult.rawContent){
+          ServiceActions().actions("login", context, qrResult.rawContent);
+        }
+        else{
+          wifiModal(qrResult.rawContent, "");  
+        }
+      });
+    }catch(e){
+      print(e);
+    }
   }
 
   Future<List<WifiResult>> getWifi() async{
@@ -172,36 +136,4 @@ class ListState extends State<PasteList>{
     return list;
   }
 
-  Future<List<Album>> fetchAlbum() async {
-    try{  
-      final response = await http.get('https://jsonplaceholder.typicode.com/albums?_start=' + start.toString() + '&_limit=10');
-      if (response.statusCode == 200) {
-        List<Album> list = (json.decode(response.body) as List).map((data) => Album.fromJson(data)).toList();
-        lista = new List.from(lista)..addAll(list);
-        return lista;
-      }
-      else{
-        return [];
-      }
-    }catch(_){
-      return [];
-    }
-  }
- 
-}
-
-class Album {
-  final int userId;
-  final int id;
-  final String title;
-
-  Album._({this.userId, this.id, this.title});
-
-  factory Album.fromJson(Map<String, dynamic> json) {
-    return new Album._(
-      userId: json['userId'],
-      id: json['id'],
-      title: json['title'],
-    );
-  }
 }
